7c31fa84012b03019cef8174cb836cac9a2eeb15
TonyPlaysGames <aht915acc@gmail.com>
2025-11-23 21:48:32 -0500

    Update logViewer.bat

--- logViewer.bat
+++ logViewer.bat
@@ -4,9 +4,6 @@ cd /d "%SCRIPT_DIR%"
 
 echo [%TIME%] [JENKINS] Starting log viewer on port 7080...
 
-REM Tell jenkins not to kill this process.
-set JENKINS_NODE_COOKIE=dontKillMe
+powershell -Command "Start-Process node -ArgumentList 'logViewer.js' -WindowStyle Normal -WorkingDirectory '%SCRIPT_DIR%'"
 
-start "Log Viewer" cmd /c "set JENKINS_NODE_COOKIE=dontKillMe && node logViewer.js"
-
-echo [%TIME%] [JENKINS] Log viewer started. Process will persist after build completes.
+echo [%TIME%] [JENKINS] Log viewer started as detached process.
