529e265c93d504f1c98903efcbb779ea642da776
TonyPlaysGames <aht915acc@gmail.com>
2025-11-23 22:13:57 -0500

    Update logViewer.bat

--- logViewer.bat
+++ logViewer.bat
@@ -2,9 +2,8 @@
 set SCRIPT_DIR=%~dp0
 cd /d "%SCRIPT_DIR%"
 
-echo [%TIME%] [JENKINS] Starting log viewer on port 7080...
+echo [%TIME%] [JENKINS] Starting log viewer on port 7080 via Task Scheduler...
 
-REM Start node process in background with Jenkins cookie set in same command
-start /B cmd /c "set JENKINS_NODE_COOKIE=dontKillMe && node logViewer.js"
+SCHTASKS /Run /TN "Livestock-LogViewer"
 
-echo [%TIME%] [JENKINS] Log viewer started as background process.
+echo [%TIME%] [JENKINS] Log viewer task triggered.
